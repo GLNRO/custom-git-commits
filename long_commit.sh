d900bf1bba5d19221c39565485258b7eac81f45e
glnro <lg@glnro.com>
2016-10-08 16:25:15 -0400

    Author 1: Ada Lovelace <alovelace@corporation.com> \nAuthor2: Kent Beck <kbeck@coproration.com> \nCard: 0 \nFirst draft of long formatting

--- long_commit.sh
+++ long_commit.sh
@@ -1,34 +1,80 @@
-AUTHOR1="author 1"
-AUTHOR2="author 2"
+INPUT1="author 1"
+INPUT2="author 2"
 CARD="card #"
 MESSAGE="commit message"
 
+ALAN=("Alan" "Kay" "<bbarker@corporation.com>")
+ADA=("Ada" "Lovelace" "<bhope@corporation.com>")
+KENT=("Kent" "Beck" "<kbeck@coproration.com>")
+GRACE=("Grace" "Hopper" "<ghopper@corporation.gov>")
+
+AUTHOR1=""
+AUTHOR2=""
+
+MEMBERS=(ALAN ADA KENT GRACE)
+
 function setAuthor1 {
-  
+  case $INPUT1 in
+    "Alan")
+      AUTHOR1="Alan Kay <akay@corporation.com>"
+    ;;
+    "Ada" )
+      AUTHOR1="Ada Lovelace <alovelace@corporation.com>"
+    ;;
+    "Kent" )
+      AUTHOR1="Kent Beck <kbeck@coproration.com>"
+    ;;
+    "Grace" )
+      AUTHOR1="Grace Hopper <ghopper@corporation.gov>"
+    ;;
+
+  esac
+
 }
 
 function setAuthor2 {
+  case $INPUT2 in
+    "Alan")
+      AUTHOR2="Alan Kay <akay@corporation.com>"
+    ;;
+    "Ada" )
+      AUTHOR2="Ada Lovelace <alovelace@corporation.com>"
+    ;;
+    "Kent" )
+      AUTHOR2="Kent Beck <kbeck@coproration.com>"
+    ;;
+    "Grace" )
+      AUTHOR2="Grace Hopper <ghopper@corporation.gov>"
+    ;;
 
-}
-
-function formatCommitMessage {
+  esac
 
 }
 
 echo "Enter first pair:"
 read input1
-AUTHOR1="${input1}"
+INPUT1="${input1}"
 echo "Enter second pair:"
 read input2
-AUTHOR2="${input2}"
+INPUT2="${input2}"
 echo "Enter card link:"
 read input3
 CARD="${input3}"
 echo "Enter commit message:"
 read input4
 MESSAGE="${input4}"
-printf "\nAuthor 1: $AUTHOR1
-Author 2: $AUTHOR2
-Card: $CARD
-$MESSAGE
-\n"
+
+
+setAuthor1
+setAuthor2
+
+COMMIT="Author 1: $AUTHOR1
+\nAuthor2: $AUTHOR2
+\nCard: $CARD
+\n$MESSAGE"
+
+git commit -m"$COMMIT"
+
+echo -e $COMMIT
+
+# echo $AUTHOR1
